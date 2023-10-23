d39f0e8383e0c6a5b09210f33cdb61f67533c810
anil-k-rajamoni18 <anil.k.rajamoni99@gmail.com>
2023-10-23 09:02:38 +0530

    oct23 commit

---
+++ CLASS_PY_FILES/Utils/example.py
@@ -0,0 +1,6 @@
+import justdoit
+ 
+print(justdoit.hi()) # methods
+print(justdoit.greet("kumar"))
+print(justdoit.username)  #() method 
+print(justdoit.password) # attributes 
---
+++ CLASS_PY_FILES/Utils/justdoit.py
@@ -0,0 +1,8 @@
+username: str = 'kumar' # variable 
+password: str = 'kumar@1234'
+
+def hi()-> str:
+    return "hi"
+
+def greet(name: str) -> str:
+    return f'hello {name}'
